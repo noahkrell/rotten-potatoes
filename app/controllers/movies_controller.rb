32342407fa7a999f0798fffa759034a375b0333f
Peter Haley <email": null>
2017-11-01 18:21:09 -0500

    search bar

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,4 +1,12 @@
 class MoviesController < ApplicationController
+
   def index
+    @movies = Movie.all
+    if params[:search]
+      @movies = Movie.search(params[:search]).order("created_at DESC")
+    else
+      @movies = Movie.all.order("created_at DESC")
+    end
   end
+
 end
