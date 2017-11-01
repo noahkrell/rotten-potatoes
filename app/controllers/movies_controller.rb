6083524005a26f31060c6024910c346dec145f8a
Noah Krell <noahkrell14@gmail.com>
2017-11-01 18:03:41 -0500

    add movie lists on homepage and link to movie page

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,4 +1,13 @@
 class MoviesController < ApplicationController
+  include MoviesHelper
   def index
+    @coming_soon = coming_soon
+    @in_theaters = in_theaters_now
+    @genres = Genre.all
   end
+
+  def show
+    @movie = Movie.find(params[:id])
+  end
+
 end
--- app/helpers/movies_helper.rb
+++ app/helpers/movies_helper.rb
@@ -1,2 +1,29 @@
 module MoviesHelper
+  def coming_soon
+    upcoming = []
+    all = Movie.all
+    all.each do |movie|
+      if movie.release > Date.today
+        upcoming << movie
+      end
+    end
+    upcoming
+  end
+
+  def by_genre
+  end
+
+  def in_theaters_now
+    in_theaters = []
+    all = Movie.all
+    all.each do |movie|
+      # this logic isn't complete
+      # shows all movies release before today as 'in theaters'
+      if movie.release < Date.today
+        in_theaters << movie
+      end
+    end
+    in_theaters
+  end
+
 end
